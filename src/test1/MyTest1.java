cda34a89560a67a95509fb02a4e896d07c3f8e50
Abdullah MOHAMMADI <ab.mohammadi791@gmail.com>
2024-08-08 18:09:48 -0400

    Initial push

---
+++ src/test1/MyTest1.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class MyTest1 {
+public static void main(String[] args) {
+    System.out.println("Hi there!");
+    System.out.println("My name is Abdullah");
+    System.out.println("I'm going to learn Java");
+}
+
+}
